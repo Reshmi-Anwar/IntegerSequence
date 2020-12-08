515c0d367744e12d2a78869fa0687f6782bec079
Reshmi-Anwar <ranwar10@stuy.edu>
2020-12-08 11:46:05 -0500

    copied values into data array

--- ArraySequence.java
+++ ArraySequence.java
@@ -9,8 +9,13 @@ public class ArraySequence implements IntegerSequence{
   }
 
   */
-  /*Construct the sequence by copying values from the other array into the data array*/
-  //public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+    data = new int[other.length];
+    for(int i = 0; i < other.length; i++){
+      data[i] = other[i];
+    }
+    currentIndex = 0;
+  }
 
 
   public boolean hasNext(){
