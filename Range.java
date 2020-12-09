df4e1ba80efe6238798b407243468f78b06478c3
Reshmi-Anwar <ranwar10@stuy.edu>
2020-12-09 17:50:24 -0500

    fixed based on tester

--- Range.java
+++ Range.java
@@ -5,8 +5,7 @@ public class Range implements IntegerSequence{
     this.start = start;
     this.end = end;
     if (end < start){
-      this.end = start;
-      this.start = end;
+      throw new IllegalArgumentException("Illegal argument");
     }
     this.current = start;
   }
@@ -14,7 +13,7 @@ public class Range implements IntegerSequence{
     current = start;
   }
   public int length(){
-    return (end - start);
+    return (end - start + 1);
   }
   public boolean hasNext(){
     return (current <= end);
